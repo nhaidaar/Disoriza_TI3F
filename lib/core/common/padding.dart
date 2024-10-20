b960fb4b9e8be840de1afb7c2d8ccc1d4d843b98
masizsa <ihza.nk.160903@gmail.com>
2024-10-20 10:33:01 +0700

    [add]: add class to set padding apps

---
+++ lib/core/common/padding.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Padding {
+  static const EdgeInsets xSmall = EdgeInsets.all(12.0);
+  static const EdgeInsets small = EdgeInsets.all(16.0);
+  static const EdgeInsets medium = EdgeInsets.all(20.0);
+  static const EdgeInsets large = EdgeInsets.all(40.0);
+}
