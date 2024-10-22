ea8af7b4e61a17ea6f47464bc27ab6b74ca75dc8
masizsa <ihza.nk.160903@gmail.com>
2024-10-22 13:53:56 +0700

    [create:page]: ceating page for each section og content

---
+++ lib/features/comunity/pages/activity_comment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ActivityComment extends StatefulWidget {
+  const ActivityComment({super.key});
+
+  @override
+  State<ActivityComment> createState() => _ActivityCommentState();
+}
+
+class _ActivityCommentState extends State<ActivityComment> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/features/comunity/pages/activity_liked.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ActivityLiked extends StatefulWidget {
+  const ActivityLiked({super.key});
+
+  @override
+  State<ActivityLiked> createState() => _ActivityLikedState();
+}
+
+class _ActivityLikedState extends State<ActivityLiked> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
