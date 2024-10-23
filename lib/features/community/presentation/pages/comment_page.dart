e4baabaad78e8fdd1c2ce18339bd69ecfbf888c0
masizsa <ihza.nk.160903@gmail.com>
2024-10-23 23:52:09 +0700

    [create:page]

--- lib/features/community/presentation/pages/comment_page.dart
+++ lib/features/community/presentation/pages/comment_page.dart
@@ -1,10 +1,26 @@
 import 'package:disoriza/core/common/colors.dart';
+import 'package:disoriza/core/common/delete_alert.dart';
+import 'package:disoriza/core/common/fontstyles.dart';
+import 'package:disoriza/features/community/presentation/widgets/comment_card.dart';
 import 'package:disoriza/features/community/presentation/widgets/post_card.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 
 class CommentPage extends StatefulWidget {
-  const CommentPage({super.key});
+  final TextEditingController? controller;
+  final FocusNode? focusNode;
+  final TextInputType? keyboardType;
+  final bool isEnabled;
+  final VoidCallback? onTap;
+
+  const CommentPage({
+    super.key,
+    this.controller,
+    this.focusNode,
+    this.keyboardType,
+    this.isEnabled = true,
+    this.onTap,
+  });
 
   @override
   State<CommentPage> createState() => _CommentPageState();
@@ -14,39 +30,126 @@ const List<String> filterMenu = <String>['Terbaru', 'Terpopuler'];
 
 class _CommentPageState extends State<CommentPage> {
   String dropdownValue = filterMenu.first;
+  String hint = 'Berikan Komentar';
+
+ void _showDeleteDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => const DeleteAlert(
+        titleMessage: "Ingin menghapus riwayat ini?",
+        detailMessage: "Setelah dihapus, data tidak dapat diurungkan.",
+      ),
+    ).then((confirmed) {
+      if (confirmed == true) {
+        // Perform delete action here
+        print('Data dihapus'); // Replace with actual delete logic
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
+      backgroundColor: neutral10,
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(),
+            const Text(
+              'Detail Diskusi',
+              style: TextStyle(fontSize: 16),
+            ),
+            IconButton(
+              icon: const Icon(IconsaxPlusLinear.trash),
+              color: dangerMain,
+              onPressed: _showDeleteDialog,
+            )
+          ],
+        ),
+        backgroundColor: neutral10,
+      ),
+      body: ListView(
+        children: [
+          const PostCard(
+            isDetailDiscussion: true,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Komentar',
+                  style: TextStyle(fontSize: 16),
+                ),
+                DropdownButton(
+                  value: dropdownValue,
+                  icon: const Icon(IconsaxPlusLinear.arrow_down),
+                  onChanged: (String? value) {
+                    setState(() {
+                      dropdownValue = value!;
+                    });
+                  },
+                  items:
+                      filterMenu.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    );
+                  }).toList(),
+                  underline: const SizedBox.shrink(),
+                  dropdownColor: neutral10,
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            child: Column(
+              children: [
+                CommentCard(),
+                CommentCard(),
+                CommentCard(),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 60,
+          )
+        ],
+      ),
+      bottomSheet: Container(
+        color: neutral10,
         padding: const EdgeInsets.all(8),
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16), color: neutral10),
-          child: Column(
-            children: [
-              PostCard(),
-              Row(
-                children: [
-                  Text('Komentar'),
-                  DropdownButton(
-                    value: dropdownValue,
-                    icon: Icon(IconsaxPlusLinear.arrow_down),
-                    onChanged: (String? value) {
-                      setState(() {
-                        dropdownValue = value!;
-                      });
-                    },
-                    items: filterMenu.map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  )
-                ],
-              )
-            ],
+        child: TextFormField(
+          focusNode: widget.focusNode,
+          controller: widget.controller,
+          keyboardType: widget.keyboardType,
+          style: mediumTS.copyWith(
+              fontSize: 14, color: widget.isEnabled ? neutral100 : neutral60),
+          decoration: InputDecoration(
+            enabled: widget.isEnabled,
+            filled: true,
+            fillColor: backgroundCanvas,
+            suffixIcon: const Icon(IconsaxPlusLinear.send_1),
+            hintText: hint,
+            hintStyle: mediumTS.copyWith(fontSize: 14, color: neutral60),
+            contentPadding: const EdgeInsets.all(16),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(40),
+              borderSide: BorderSide.none,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(40),
+              borderSide: BorderSide.none,
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(40),
+            ),
           ),
         ),
       ),
