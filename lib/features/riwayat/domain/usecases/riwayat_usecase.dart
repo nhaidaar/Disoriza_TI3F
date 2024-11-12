74248605000015bbea80cdc743da230acca0f260
NadilaAmaliaaa <nadilaamaliapribadi@gmail.com>
2024-11-12 17:34:19 +0700

    [create]: riwayat usecases

---
+++ lib/features/riwayat/domain/usecases/riwayat_usecase.dart
@@ -0,0 +1,34 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../../data/models/riwayat_model.dart';
+import '../../data/models/disease_model.dart';
+import '../repositories/riwayat_repository.dart';
+
+class RiwayatUsecase {
+  final RiwayatRepository _RiwayatRepository;
+  const RiwayatUsecase(this._RiwayatRepository);
+
+    Future<Either<AppwriteException, List<RiwayatModel>>> fetchAllRiwayat({
+      required User user,
+      bool latest = false,
+    }) async {
+      return await _RiwayatRepository.fetchAllRiwayat(
+        user: user,
+        latest: latest,
+      );
+    }
+    Future<Either<AppwriteException, void>> deleteRiwayat({
+      required String histId,
+    }) {
+      return _RiwayatRepository.deleteRiwayat(histId: histId);
+    }
+    Future<Either<AppwriteException, DiseaseModel>> fetchDisease({
+      required String id_disease,
+    }) {
+      return _RiwayatRepository.fetchDisease(
+        id_disease: id_disease,
+      );
+    }
+}
