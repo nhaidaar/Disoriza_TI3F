f1c9137058970319ef922e37b934e32da1b526a2
NadilaAmaliaaa <nadilaamaliapribadi@gmail.com>
2024-11-12 17:33:45 +0700

    [create]: riwayat domain repositories

---
+++ lib/features/riwayat/domain/repositories/riwayat_repository.dart
@@ -0,0 +1,19 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../../data/models/riwayat_model.dart';
+import '../../data/models/disease_model.dart';
+
+abstract class RiwayatRepository {
+  Future<Either<AppwriteException, List<RiwayatModel>>> fetchAllRiwayat({
+    required User user,
+    bool latest = false,
+  });
+  Future<Either<AppwriteException, void>> deleteRiwayat({
+    required String histId,
+  });
+  Future<Either<AppwriteException, DiseaseModel>> fetchDisease({
+    required String id_disease,
+  });
+}
